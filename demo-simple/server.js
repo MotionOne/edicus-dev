dd1f924b5da799201aa3a697285434b9be2a8eb4
harry <harry@motion1.co.kr>
2025-11-12 17:57:09 +0900

    refactor: callEdicusAPI()를 공통 사용하도록 변경

--- demo-simple/server.js
+++ demo-simple/server.js
@@ -13,189 +13,117 @@
 */
 import { server_env } from '../.env.js';
 
-export function get_custom_token__from_server(user_id, callback) {
-	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
-	// https://docs.google.com/document/d/1buvh-TjQtAqddAD4-QFxBHKFDESRxInsxFcViuEwNZc/edit#heading=h.lu754uc9u7g
-	var url = server_env.apiHost + '/api/auth/token';
+/**
+ * Edicus API 호출을 위한 공통 함수
+ * @param {string} url - API endpoint URL
+ * @param {string} method - HTTP method (GET, POST, DELETE 등)
+ * @param {object} additionalHeaders - 추가 헤더 (edicus-uid, edicus-email 등)
+ * @param {object} data - 요청 데이터
+ * @param {function} callback - 콜백 함수
+ */
+function callEdicusAPI(url, method, additionalHeaders = {}, data = {}, callback) {
+	const headers = {
+		'edicus-api-key': server_env.apiKey,
+		'Content-Type': 'application/json',
+		...additionalHeaders
+	};
+
 	$.ajax({
 		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type': 'application/json'
-		},
-		method: 'POST', 
+		headers: headers,
+		method: method,
 		dataType: 'json',
-		data: {},
+		data: method === 'GET' ? undefined : (Object.keys(data).length > 0 ? JSON.stringify(data) : {}),
 		success: function (data) {
 			console.log(data);
 			callback(null, data);
+		},
+		error: function(xhr, status, error) {
+			console.error('API Error:', error);
+			callback(error, null);
 		}
-	})
+	});
+}
+
+export function get_custom_token__from_server(user_id, callback) {
+	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
+	// https://docs.google.com/document/d/1buvh-TjQtAqddAD4-QFxBHKFDESRxInsxFcViuEwNZc/edit#heading=h.lu754uc9u7g
+	const url = server_env.apiHost + '/api/auth/token';
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'POST', additionalHeaders, {}, callback);
 }
 
 
 export function get_custom_token_of_staff__from_server(staff_info, callback) {
 	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
 	// https://docs.google.com/document/d/1buvh-TjQtAqddAD4-QFxBHKFDESRxInsxFcViuEwNZc/edit#heading=h.5p6krexjtpgi
-	var url = server_env.apiHost + '/api/auth/staff/token';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-email': staff_info.email,
-			'edicus-pwd': staff_info.pwd,
-			'Content-Type': 'application/json'
-		},
-		method: 'POST',
-		dataType: 'json',
-		data: {},
-		success: function (data) {
-			console.log(data);
-			callback(null, data);
-		}
-	})
+	const url = server_env.apiHost + '/api/auth/staff/token';
+	const additionalHeaders = {
+		'edicus-email': staff_info.email,
+		'edicus-pwd': staff_info.pwd
+	};
+	callEdicusAPI(url, 'POST', additionalHeaders, {}, callback);
 }
 
 export function get_project_list__from_server(user_id, callback) {
 	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
 	// https://docs.google.com/document/d/1buvh-TjQtAqddAD4-QFxBHKFDESRxInsxFcViuEwNZc/edit#heading=h.gqp5xb7uwjxa    
-	var url = server_env.apiHost + '/api/projects';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type':'application/json'
-		},
-		method: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-			callback(null, data);
-		}
-	})
+	const url = server_env.apiHost + '/api/projects';
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'GET', additionalHeaders, {}, callback);
 }
 
 export function clone_project__from_server(user_id, project_id, callback) {
 	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
 	// https://docs.google.com/document/d/1OhWdgv9Sz8By4N48eY0uO_84M3keLVQvdpdpK9u_3bA/edit#heading=h.12nm233v8th7
-	
-	var url = server_env.apiHost + '/api/projects/' + project_id +'/clone';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type':'application/json'
-		},
-		method: 'POST',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-			callback(data);
-		}
-	})
+	const url = server_env.apiHost + '/api/projects/' + project_id + '/clone';
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'POST', additionalHeaders, {}, (err, data) => {
+		callback(data);
+	});
 }
 
 export function delete_project__from_server(user_id, project_id, callback) {
 	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
 	// https://docs.google.com/document/d/1buvh-TjQtAqddAD4-QFxBHKFDESRxInsxFcViuEwNZc/edit#heading=h.3uglpt2407ob
-	
-	var url = server_env.apiHost + '/api/projects/' + project_id;
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type':'application/json'
-		},
-		method: 'DELETE',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-			callback(data.err);
-		}
-	})
+	const url = server_env.apiHost + '/api/projects/' + project_id;
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'DELETE', additionalHeaders, {}, (err, data) => {
+		callback(data.err);
+	});
 }
 
 export function tentative_order_project__from_server(user_id, project_id, order, callback) {
 	console.log(user_id, project_id, order);
-
-	var url = server_env.apiHost + '/api/projects/' + project_id + '/order/tentative';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type':'application/json'
-		},
-		method: 'POST',
-		dataType: 'json',
-		data: JSON.stringify(order),
-		success: function(data) {
-			console.log(data);
-			callback(data.err);
-		}
-	})    
+	const url = server_env.apiHost + '/api/projects/' + project_id + '/order/tentative';
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'POST', additionalHeaders, order, (err, data) => {
+		callback(data.err);
+	});
 }    
 
 export function definitive_order_project__from_server(user_id, project_id, callback) {
 	console.log(user_id, project_id);
-
-	var url = server_env.apiHost + '/api/projects/' + project_id + '/order/definitive';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type':'application/json'
-		},
-		method: 'POST',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-			callback(data.err);
-		}
-	})    
+	const url = server_env.apiHost + '/api/projects/' + project_id + '/order/definitive';
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'POST', additionalHeaders, {}, (err, data) => {
+		callback(data.err);
+	});
 }    
 
 export function cancel_order_project__from_server(user_id, order_id, callback) {
 	console.log(user_id, order_id);
-
-	var url = server_env.apiHost + '/api/orders/' + order_id + '/cancel';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'edicus-uid': user_id,
-			'Content-Type':'application/json'
-		},
-		method: 'POST',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-			callback(data.err);
-		}
-	})    
+	const url = server_env.apiHost + '/api/orders/' + order_id + '/cancel';
+	const additionalHeaders = { 'edicus-uid': user_id };
+	callEdicusAPI(url, 'POST', additionalHeaders, {}, (err, data) => {
+		callback(data.err);
+	});
 }    
 
 
 export function get_preview_urls__from_server(project_id, callback) {
 	// Edicus Server API 구글독스 문서 (문서접근에 권한이 필요하니, 바로 열리지 않으면 연락바랍니다.)
 	// https://docs.google.com/document/d/1buvh-TjQtAqddAD4-QFxBHKFDESRxInsxFcViuEwNZc/edit#heading=h.1pl8dok8mgjo
-	var url = server_env.apiHost + '/api/projects/' + project_id + '/preview_urls';
-	$.ajax({
-		url: url,
-		headers: {
-			'edicus-api-key': server_env.apiKey,
-			'Content-Type':'application/json'
-		},
-		method: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-			callback(null, data);
-		}
-	})
+	const url = server_env.apiHost + '/api/projects/' + project_id + '/preview_urls';
+	callEdicusAPI(url, 'GET', {}, {}, callback);
 }
